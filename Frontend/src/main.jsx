b7b4588f5597c8d002a2196eb6c8738646aecdbe
Frany28 <franciely.vivas@gmail.com>
2026-03-31 11:16:06 -0400

    feat: configure BrowserRouter with dynamic basename and set base path in Vite config

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { Home } from "./pages/pages.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
